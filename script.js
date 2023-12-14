459cd935d9992191513d85e57af6144f310af6e4
Pablo <pablo-nahuel@hotmail.com>
2023-12-14 09:12:12 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function registroEntrada() {
+    // Aquí va el código para registrar la entrada del usuario
+    alert("Registro de entrada, complete los siguientes campos...");
+  }
+  function registroSalida() {
+    // Aquí va el código para registrar la salida del usuario
+    alert("Registro de salida, complete los siguientes campos...");
+  }
